7ac9f5ce8a2427b8171acb1ceceba2399128290c
luokai <luokai@bluemoon.com.cn>
2016-07-13 15:48:08 +0800

    详情刷新页面抽象（未完成）

--- src/cn/com/bluemoon/lib/utils/LibViewUtil.java
+++ src/cn/com/bluemoon/lib/utils/LibViewUtil.java
@@ -10,78 +10,110 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import cn.com.bluemoon.lib.qrcode.R;
+
 public class LibViewUtil {
 
 
-	public static void setListViewHeight2(ListView listView) { 
-	    if(listView == null) return;
+    public static void setListViewHeight2(ListView listView) {
+        if (listView == null) return;
 
-	    ListAdapter listAdapter = listView.getAdapter(); 
-	    if (listAdapter == null) { 
-	        // pre-condition 
-	        return; 
-	    } 
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
 
-	    int totalHeight = 0; 
-	    int desiredWidth = MeasureSpec.makeMeasureSpec(
-	    		listView.getWidth(), MeasureSpec.AT_MOST);
-	    for (int i = 0; i < listAdapter.getCount(); i++) { 
-	        View listItem = listAdapter.getView(i, null, listView); 
-	        listItem.measure(desiredWidth, 0); 
-	        totalHeight += listItem.getMeasuredHeight(); 
-	    } 
+        int totalHeight = 0;
+        int desiredWidth = MeasureSpec.makeMeasureSpec(
+                listView.getWidth(), MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
 
-	    ViewGroup.LayoutParams params = listView.getLayoutParams(); 
-	    params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1)); 
-	    listView.setLayoutParams(params); 
-	}
-	
-
-	public static void setListViewHeight(ListView listView) { 
-	    if(listView == null) return;
-
-	    ListAdapter listAdapter = listView.getAdapter(); 
-	    if (listAdapter == null) { 
-	        // pre-condition 
-	        return; 
-	    } 
-
-	    int totalHeight = 0; 
-	    for (int i = 0; i < listAdapter.getCount(); i++) { 
-	        View listItem = listAdapter.getView(i, null, listView); 
-	        listItem.measure(0, 0); 
-	        totalHeight += listItem.getMeasuredHeight(); 
-	    } 
-
-	    ViewGroup.LayoutParams params = listView.getLayoutParams(); 
-	    params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1)); 
-	    listView.setLayoutParams(params); 
-	}
-	
-
-	public static void hideIM(View v) {
-		try {
-			InputMethodManager im = (InputMethodManager) v.getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
-			IBinder windowToken = v.getWindowToken();
-			if (windowToken != null) {
-				im.hideSoftInputFromWindow(windowToken, 0);
-			}
-		} catch (Exception e) {
-
-		}
-	}
-	
-	public static void hideKeyboard(View v) {
-		InputMethodManager imm = (InputMethodManager) v.getContext()
-				.getSystemService(Context.INPUT_METHOD_SERVICE);     
-        if (imm.isActive()) {     
-            imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);   
-        }  
-    }
-	public static void showKeyboard(View v) {
-		InputMethodManager imm = (InputMethodManager)v.getContext()
-				.getSystemService(Context.INPUT_METHOD_SERVICE);     
-        imm.showSoftInput(v,InputMethodManager.SHOW_FORCED);    
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
     }
 
+
+    public static void setListViewHeight(ListView listView) {
+        if (listView == null) return;
+
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
+
+
+    public static void hideIM(View v) {
+        try {
+            InputMethodManager im = (InputMethodManager) v.getContext().getSystemService(Activity
+                    .INPUT_METHOD_SERVICE);
+            IBinder windowToken = v.getWindowToken();
+            if (windowToken != null) {
+                im.hideSoftInputFromWindow(windowToken, 0);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static void hideKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+        }
+    }
+
+    public static void showKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(v, InputMethodManager.SHOW_FORCED);
+    }
+
+    /**
+     * 设置控件可见性
+     */
+    public static void setViewVisibility(View view, int visibility) {
+        if (view != null && view.getVisibility() != visibility) {
+            view.setVisibility(visibility);
+        }
+    }
+
+    /**
+     * 递归设置子控件的enable
+     */
+    public static void setChildEnableRecursion(ViewGroup layout, boolean isEnable) {
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View child = layout.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                setChildEnableRecursion((ViewGroup) child, isEnable);
+            } else {
+                if (isEnable) {
+                    Boolean enable = (Boolean) child.getTag(R.id.tag_ori_enable);
+                    child.setEnabled(enable != null ? enable : isEnable);
+                } else {
+                    child.setTag(R.id.tag_ori_enable, child.isEnabled());
+                    child.setEnabled(false);
+                }
+            }
+        }
+    }
 }
