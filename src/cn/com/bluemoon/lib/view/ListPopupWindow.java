31095c625ee9069ce1c8acf28ecd17c72f41aa86
chenchongsen <chenchongsen@bluemoon.com.cn>
2016-07-15 17:33:21 +0800

    优化自定义搜索框控件，修改所有用到的类，改动较大，需着重测试一下；

---
+++ src/cn/com/bluemoon/lib/view/ListPopupWindow.java
@@ -0,0 +1,150 @@
+
+package cn.com.bluemoon.lib.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.com.bluemoon.lib.qrcode.R;
+
+public class ListPopupWindow extends PopupWindow {
+
+    private Context mContext;
+    private View view;
+    private OnSelectListener listener;
+    private HistoryListAdapter adapter;
+    private ListView listView;
+
+    public ListPopupWindow(Context context,OnSelectListener listener) {
+        this.mContext = context;
+        this.listener = listener;
+        init();
+    }
+
+    private void init() {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.layout_search_history, null);
+        setWidth(LayoutParams.MATCH_PARENT);
+        setHeight(LayoutParams.MATCH_PARENT);
+//        setFocusable(true);
+        setOutsideTouchable(false);
+        setInputMethodMode(INPUT_METHOD_NEEDED);
+        setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        setContentView(view);
+        setBackgroundDrawable(null);
+        listView = (ListView) view.findViewById(R.id.listView_history);
+        view.startAnimation(AnimationUtils.loadAnimation(mContext,
+                R.anim.push_top_enter));
+
+    }
+
+    private void setData(List<String> listHistory){
+        if(adapter==null){
+            adapter = new HistoryListAdapter(mContext);
+        }
+        adapter.setList(listHistory);
+        if(listView.getAdapter()==null){
+            listView.setAdapter(adapter);
+        }else{
+            adapter.notifyDataSetChanged();
+        }
+
+    }
+
+    public void showPopwindow(View popStart,List<String> list) {
+        setData(list);
+        showAsDropDown(popStart);
+    }
+
+    public interface OnSelectListener {
+        void select(String str);
+        void delete(List<String> list);
+    }
+
+    class HistoryListAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+        private List<String> list;
+
+        public HistoryListAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        public void setList(List<String> list){
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            if (list == null) {
+                return 0;
+            }
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder viewHolder = null;
+            if(viewHolder==null){
+                viewHolder = new ViewHolder();
+                convertView = mInflater.inflate(R.layout.item_search_history, null);
+                viewHolder.txtContent = (TextView) convertView.findViewById(R.id.txt_content);
+                viewHolder.imgDel = (ImageView) convertView.findViewById(R.id.img_delete);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder)convertView.getTag();
+            }
+
+            viewHolder.txtContent.setText(list.get(position).toString());
+            viewHolder.imgDel.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    list.remove(position);
+                    notifyDataSetChanged();
+                    if(listener!=null){
+                        listener.delete(list);
+                    }
+                }
+            });
+            convertView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener!=null){
+                        listener.select(list.get(position));
+                    }
+                }
+            });
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView txtContent;
+            ImageView imgDel;
+        }
+
+    }
+}
--- src/cn/com/bluemoon/lib/view/SearchViewOld.java
+++ src/cn/com/bluemoon/lib/view/SearchViewOld.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,7 +29,7 @@ import cn.com.bluemoon.lib.utils.LibViewUtil;
 /**
  * Created by bm on 2016/6/20.
  */
-public class CommonSearchView extends LinearLayout implements ListPopupWindow.OnSelectListener {
+public class SearchViewOld extends LinearLayout {
 
     private Context context;
     private SearchViewListener listener;
@@ -39,20 +41,19 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
     private String search;
     private String hint;
     private int textColor = 0;
+    private CommonAdapter adapter;
     private View emptyView;
     private List<String> listHistory;
     private int maxSize = 5;
     private boolean isSearchEmpty = true;
-    private ListPopupWindow popupWindow;
-    private LinearLayout layoutTitle;
 
 
-    public CommonSearchView(Context context) {
+    public SearchViewOld(Context context) {
         super(context);
         init(context, null);
     }
 
-    public CommonSearchView(Context context, AttributeSet attrs) {
+    public SearchViewOld(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = context.obtainStyledAttributes(attrs,
                 R.styleable.CommonSearchView);
@@ -64,18 +65,20 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
         LayoutInflater layoutInflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        layoutInflater.inflate(R.layout.layout_search, this);
+        layoutInflater.inflate(R.layout.layout_search_old, this);
 
         if (typedArray != null) {
             textColor = typedArray.getInt(R.styleable.CommonSearchView_text_color, 0);
             search = typedArray.getString(R.styleable.CommonSearchView_text_ok);
             cancel = typedArray.getString(R.styleable.CommonSearchView_text_cancel);
             hint = typedArray.getString(R.styleable.CommonSearchView_text_hint);
-            isSearchEmpty = typedArray.getBoolean(R.styleable.CommonSearchView_search_empty, true);
+            isSearchEmpty = typedArray.getBoolean(R.styleable.CommonSearchView_search_empty,true);
         }
+
         etSearch = (CommonClearEditText) this.findViewById(R.id.et_search);
         txtSearch = (TextView) this.findViewById(R.id.txt_search);
         listView = (ListView) this.findViewById(R.id.listView_history);
+//        setEmptyView(new CommonEmptyView(context).setContentText(context.getString(R.string.history_no_data)).setRefreshable(false));
         etSearch.setOnKeyListener(onKeyListener);
         etSearch.setCallBack(editTextCallBack);
         if (StringUtils.isEmpty(search)) {
@@ -100,10 +103,11 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
                 }
             }
         });
-        if (popupWindow == null) {
-            popupWindow = new ListPopupWindow(context, this);
-        }
 
+    }
+
+    public CommonClearEditText getSearchEdittext() {
+        return etSearch;
     }
 
     public void setEmptyView(View view) {
@@ -112,7 +116,7 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
         listView.setEmptyView(emptyView);
     }
 
-    public String getText() {
+    public String getText(){
         return etSearch.getText().toString().trim();
     }
 
@@ -132,57 +136,49 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
         this.listHistory = listHistory;
     }
 
-    public void search() {
+    public void search(){
         String str = etSearch.getText().toString().trim();
-        if (!isSearchEmpty && StringUtils.isEmpty(str)) {
-            LibPublicUtil.showToast(context, context.getString(R.string.search_cannot_empty));
+        if(!isSearchEmpty&&StringUtils.isEmpty(str)){
+            LibPublicUtil.showToast(context,context.getString(R.string.search_cannot_empty));
             return;
         }
         etSearch.clearFocus();
-        if (listHistory == null) {
+        if(listHistory==null){
             listHistory = new ArrayList<>();
         }
         addHistory(str);
-        if (listener != null) {
-            listener.onSearch(this, str);
-        }
+        if(listener!=null) listener.onSearch(str);
     }
 
-    public void cancel() {
+    public void cancel(){
         etSearch.clearFocus();
-        if (listener != null) {
-            listener.onCancel(this);
-        }
+        if(listener!=null) listener.onCancel();
     }
 
-    public void setFocus(boolean isFocus) {
-        if (isFocus) {
+    public void setFocus(boolean isFocus){
+        if(isFocus){
             etSearch.requestFocus();
-        } else {
+        }else{
             etSearch.clearFocus();
         }
     }
 
-    public CommonClearEditText getSearchEdittext() {
-        return etSearch;
-    }
-
-    public void setSearchEmpty(boolean isSearchEmpty) {
+    public void setSearchEmpty(boolean isSearchEmpty){
         this.isSearchEmpty = isSearchEmpty;
     }
 
-    public void setHistorySize(int maxSize) {
+    public void setHistorySize(int maxSize){
         this.maxSize = maxSize;
     }
 
-    public List<String> addHistory(String str) {
-        if (listHistory == null) {
+    public List<String> addHistory(String str){
+        if(listHistory==null){
             listHistory = new ArrayList<>();
         }
-        if (!StringUtils.isEmpty(str.trim())) {
-            listHistory.add(0, str);
-            for (int i = 1; i < listHistory.size(); i++) {
-                if (i >= maxSize || listHistory.get(i).equals(str)) {
+        if(!StringUtils.isEmpty(str.trim())){
+            listHistory.add(0,str);
+            for(int i=1;i<listHistory.size();i++){
+                if(i>=maxSize||listHistory.get(i).equals(str)){
                     listHistory.remove(i);
                     i--;
                 }
@@ -192,29 +188,36 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
     }
 
     public void showHistoryView() {
-        if (popupWindow != null) {
-            popupWindow.showPopwindow(this, listHistory);
+        if (adapter == null) {
+            adapter = new CommonAdapter(context);
+        }
+        if(listView.getAdapter()==null){
+            listView.setAdapter(adapter);
+        }else{
+            adapter.notifyDataSetChanged();
+        }
+
+        if(LibViewUtil.getViewVisibility(listView.getEmptyView())==View.GONE){
+            LibViewUtil.setViewVisibility(listView,View.VISIBLE);
         }
     }
 
     public void hideHistoryView() {
-        if (popupWindow != null) {
-            popupWindow.dismiss();
-        }
-
+        LibViewUtil.setViewVisibility(listView.getEmptyView(),View.GONE);
+        LibViewUtil.setViewVisibility(listView,View.GONE);
     }
 
-    public View getHistoryView() {
+    public View getHistoryView(){
         return listView;
     }
 
-    private void checkSearchState() {
+    private void checkSearchState(){
         if (etSearch.getText().toString().length() > 0) {
             if (!txtSearch.getText().toString().equals(search)) {
                 txtSearch.setText(search);
             }
             isSearch = true;
-        } else {
+        } else{
             if (!txtSearch.getText().toString().equals(cancel)) {
                 txtSearch.setText(cancel);
             }
@@ -226,7 +229,7 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
         @Override
         public void afterTextChanged(Editable s) {
             super.afterTextChanged(s);
-            if (etSearch.isFocused()) {
+            if (etSearch.isFocused()){
                 checkSearchState();
             }
         }
@@ -244,7 +247,6 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
                 showHistoryView();
             } else {
                 LibViewUtil.hideIM(v);
-                hideHistoryView();
                 if (txtSearch.getVisibility() == View.VISIBLE) {
                     txtSearch.setVisibility(View.GONE);
                 }
@@ -264,27 +266,82 @@ public class CommonSearchView extends LinearLayout implements ListPopupWindow.On
         }
     };
 
+    class CommonAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+        public CommonAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            if (listHistory == null) {
+                return 0;
+            }
+            return listHistory.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listHistory.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder viewHolder = null;
+            if(viewHolder==null){
+                viewHolder = new ViewHolder();
+                convertView = mInflater.inflate(R.layout.item_search_history, null);
+                viewHolder.txtContent = (TextView) convertView.findViewById(R.id.txt_content);
+                viewHolder.imgDel = (ImageView) convertView.findViewById(R.id.img_delete);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder)convertView.getTag();
+            }
+
+            viewHolder.txtContent.setText(listHistory.get(position).toString());
+            viewHolder.imgDel.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listHistory.remove(position);
+                    notifyDataSetChanged();
+                }
+            });
+            convertView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setText(listHistory.get(position));
+                    etSearch.setSelection(listHistory.get(position).length());
+                    search();
+                }
+            });
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView txtContent;
+            ImageView imgDel;
+        }
+
+    }
+
     public void setSearchViewListener(final SearchViewListener listener) {
         this.listener = listener;
     }
 
-    @Override
-    public void select(String str) {
-        setText(str);
-        etSearch.setSelection(str.length());
-        search();
-    }
-
-    @Override
-    public void delete(List<String> list) {
-        listHistory = list;
-    }
-
     public interface SearchViewListener {
 
-        void onSearch(CommonSearchView view, String str);
+        void onSearch(String str);
 
-        void onCancel(CommonSearchView view);
+        void onCancel();
 
     }
 }
