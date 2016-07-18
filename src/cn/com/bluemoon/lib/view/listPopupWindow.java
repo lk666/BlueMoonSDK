eb4f292805cddbe1c2c6574f98e47a3afa1427f5
chenchongsen <chenchongsen@bluemoon.com.cn>
2016-07-18 08:24:02 +0800

    优化搜索框的历史页；

--- src/cn/com/bluemoon/lib/view/listPopupWindow.java
+++ src/cn/com/bluemoon/lib/view/listPopupWindow.java
@@ -1,140 +1,150 @@
-/** 
- * Description:
- * Copyright: Copyright (c) 2016
- * Company: BLUE MOON
- * @author: Eric Liang 
- * @version 1.0
- * @date: 2016/3/5
- */
+
 package cn.com.bluemoon.lib.view;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
-import cn.com.bluemoon.delivery.R;
-import cn.com.bluemoon.delivery.ui.DateTextView;
-import cn.com.bluemoon.delivery.utils.DateUtil;
-import cn.com.bluemoon.delivery.utils.PublicUtil;
+import java.util.List;
 
-public class listPopupWindow extends PopupWindow {
+import cn.com.bluemoon.lib.qrcode.R;
 
-	private Context mContext;
-	private DateTextView txtEndDate;
-	private TextView txtContent;
-	private Button okBtn;
-	private View view;
-	private ChooseDateListener listener;
-	private String communityCode;
-	private String bpCode;
-	private long minDate;
-	private long maxDate;
-	private long curDate;
+public class ListPopupWindow extends PopupWindow {
 
-	public listPopupWindow(Context context, String bpCode, String bpName, String communityCode, ChooseDateListener listener) {
-		this.mContext = context;
-		this.listener = listener;
-		this.bpCode = bpCode;
-		this.communityCode = communityCode;
-		init(PublicUtil.getStringParams(bpCode,bpName));
-	}
+    private Context mContext;
+    private View view;
+    private OnSelectListener listener;
+    private HistoryListAdapter adapter;
+    private ListView listView;
 
-	public listPopupWindow(Context context, ChooseDateListener listener) {
-		this.mContext = context;
-		this.listener = listener;
-		init(null);
-	}
+    public ListPopupWindow(Context context,OnSelectListener listener) {
+        this.mContext = context;
+        this.listener = listener;
+        init();
+    }
 
-	private void init(String content) {
-		LayoutInflater inflater = LayoutInflater.from(mContext);
-		view = inflater.inflate(R.layout.dialog_team_unlock_date, null);
+    private void init() {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.layout_search_history, null);
+        setWidth(LayoutParams.MATCH_PARENT);
+        setHeight(LayoutParams.MATCH_PARENT);
+//        setFocusable(true);
+        setOutsideTouchable(false);
+        setInputMethodMode(INPUT_METHOD_NEEDED);
+        setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        setContentView(view);
+        setBackgroundDrawable(null);
+        listView = (ListView) view.findViewById(R.id.listView_history);
+        view.startAnimation(AnimationUtils.loadAnimation(mContext,
+                R.anim.push_top_enter));
 
+    }
 
-		LinearLayout ll_popup = (LinearLayout) view
-				.findViewById(R.id.layout_choose_date);
+    private void setData(List<String> listHistory){
+        if(adapter==null){
+            adapter = new HistoryListAdapter(mContext);
+        }
+        adapter.setList(listHistory);
+        if(listView.getAdapter()==null){
+            listView.setAdapter(adapter);
+        }else{
+            adapter.notifyDataSetChanged();
+        }
 
-		setWidth(LayoutParams.MATCH_PARENT);
-		setHeight(LayoutParams.WRAP_CONTENT);
-		setFocusable(true);
-		setOutsideTouchable(true);
-		setContentView(view);
-		setBackgroundDrawable(mContext.getResources().getDrawable(
-				R.drawable.bg_transparent));
+    }
 
-		ll_popup.startAnimation(AnimationUtils.loadAnimation(mContext,
-				R.anim.push_top_in));
+    public void showPopwindow(View popStart,List<String> list) {
+        setData(list);
+        showAsDropDown(popStart);
+    }
 
-		txtContent = ((TextView) view.findViewById(R.id.txt_name));
-		txtEndDate = (DateTextView) view.findViewById(R.id.txt_end_date);
-		txtContent.setText(content);
-		okBtn = (Button) view.findViewById(R.id.btn_confirm);
-		okBtn.setOnClickListener(onclicker);
-		view.setOnClickListener(onclicker);
-		txtEndDate.setCallBack(callBack);
-	}
+    public interface OnSelectListener {
+        void select(String str);
+        void delete(List<String> list);
+    }
 
-	OnClickListener onclicker = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			if(v == okBtn){
-				if(listener!=null)
-				listener.callBack(bpCode,communityCode,curDate);
-				dismiss();
-			}else if(v == view){
-				dismiss();
-			}
-		}
-	};
+    class HistoryListAdapter extends BaseAdapter {
 
-	DateTextView.DateTextViewCallBack callBack = new DateTextView.DateTextViewCallBack() {
-		@Override
-		public void onDate(View view, long returnDate) {
-			curDate = returnDate;
-		}
-	};
+        private LayoutInflater mInflater;
+        private List<String> list;
 
-	public void showPopwindow(View popStart) {
-		if(minDate>0&&minDate>System.currentTimeMillis()){
-			txtEndDate.setText(DateUtil.getTime(minDate, "yyyy-MM-dd"));
-			curDate = minDate;
-		}else if(maxDate>0&&maxDate<System.currentTimeMillis()) {
-			txtEndDate.setText(DateUtil.getTime(maxDate, "yyyy-MM-dd"));
-			curDate = maxDate;
-		}else{
-			txtEndDate.setText(DateUtil.getCurDate());
-			curDate = System.currentTimeMillis();
-		}
-		showAsDropDown(popStart);
-	}
+        public HistoryListAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+        }
 
-	public void setData(String bpCode,String bpName,String communityCode){
-		this.bpCode = bpCode;
-		this.communityCode = communityCode;
-		txtContent.setText(PublicUtil.getStringParams(bpCode,bpName));
-	}
+        public void setList(List<String> list){
+            this.list = list;
+        }
 
-	public void setMinDate(long minDate) {
-		this.minDate = minDate;
-		txtEndDate.updateMinDate(minDate);
-	}
+        @Override
+        public int getCount() {
+            if (list == null) {
+                return 0;
+            }
+            return list.size();
+        }
 
-	public void setMaxDate(long maxDate) {
-		this.maxDate = maxDate;
-		txtEndDate.updateMaxDate(maxDate);
-	}
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
 
-	public long getDate() {
-		return curDate;
-	}
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
-	public interface ChooseDateListener {
-		public void callBack(String bpCode, String commonityCode, long endTime);
-	}
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder viewHolder = null;
+            if(viewHolder==null){
+                viewHolder = new ViewHolder();
+                convertView = mInflater.inflate(R.layout.item_search_history, null);
+                viewHolder.txtContent = (TextView) convertView.findViewById(R.id.txt_content);
+                viewHolder.imgDel = (ImageView) convertView.findViewById(R.id.img_delete);
+                convertView.setTag(viewHolder);
+            }else{
+                viewHolder = (ViewHolder)convertView.getTag();
+            }
+
+            viewHolder.txtContent.setText(list.get(position).toString());
+            viewHolder.imgDel.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    list.remove(position);
+                    notifyDataSetChanged();
+                    if(listener!=null){
+                        listener.delete(list);
+                    }
+                }
+            });
+            convertView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(listener!=null){
+                        listener.select(list.get(position));
+                    }
+                }
+            });
+
+            return convertView;
+        }
+
+        class ViewHolder{
+            TextView txtContent;
+            ImageView imgDel;
+        }
+
+    }
 }
