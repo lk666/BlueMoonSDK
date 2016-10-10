0b1e3aacd91da8601825042be615b5115cccba41
chenchongsen <chenchongsen@bluemoon.com.cn>
2016-10-10 11:16:15 +0800

    图片二维码展示空间兼容；

--- src/cn/com/bluemoon/lib/callback/ImageDialogCallback.java
+++ src/cn/com/bluemoon/lib/callback/ImageDialogCallback.java
@@ -1,7 +1,17 @@
 package cn.com.bluemoon.lib.callback;
 
+import android.graphics.Bitmap;
+import android.view.View;
+
 public abstract class ImageDialogCallback {
 
-	public void showResult(){}
-	public void dismissResult(){}
+    public void showResult() {
+    }
+
+    public void dismissResult() {
+    }
+
+    public boolean longClick(View view,Bitmap bit) {
+        return false;
+    }
 }
